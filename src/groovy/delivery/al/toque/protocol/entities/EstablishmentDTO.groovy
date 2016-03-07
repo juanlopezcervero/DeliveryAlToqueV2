498820f886df3ec91f02c2b8a0910ea84ec79a41
jlopezcervero-technisys <jlopezcervero@technisys.com>
2016-03-07 11:34:07 -0300

    test

--- src/groovy/delivery/al/toque/protocol/entities/EstablishmentDTO.groovy
+++ src/groovy/delivery/al/toque/protocol/entities/EstablishmentDTO.groovy
@@ -1,5 +1,5 @@
 package delivery.al.toque.protocol.entitiesimport delivery.al.toque.DeliveryAreasData;import delivery.al.toque.LocationData;import delivery.al.toque.protocol.EntityDTO;
 
 class EstablishmentDTO extends EntityDTO {
-	String parentId		LocationData location		String state	String city	String postalCode	String street		String detail	String name	String hasTag	String phone		DeliveryAreasData deliveryAreas		String openingTimes	
+	String parentId		LocationData location			String state	String city	String postalCode	String street		String detail	String name	String hasTag	String phone		DeliveryAreasData deliveryAreas		String openingTimes	
 }
